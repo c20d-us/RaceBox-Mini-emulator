064981c9a57e4e2868b293adeb36e90399c151a6
Chris Halstead <84101615+c20d-us@users.noreply.github.com>
2026-06-24 16:43:39 -0700

    Update license headers

--- src/Gnimu/Gnimu.ino
+++ src/Gnimu/Gnimu.ino
@@ -1,6 +1,21 @@
-// This code is an evolution of work done by Anchit Chandra Sekhart
-// https://github.com/anchit92/Open-Source-RaceBox-mini-Emulator
+// Gnimu - RaceBox Mini-compatible GNSS+IMU streaming telemetry
+// Copyright (C) 2026 Chris Halstead
+// Based on the Open-Source RaceBox Mini Emulator by Anchit Chandra Sekhar
+// (https://github.com/anchit92/Open-Source-RaceBox-mini-Emulator)
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Settings live in config.h
 // Hardware & protocol logic lives in the ble, gnss, imu, and telemetry modules
 #include "ble.h"
